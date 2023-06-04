a0584b0ffd1378b65df8a7fa2d7e2b094dabed6e
Rashmi160403 <127473524+Rashmi160403@users.noreply.github.com>
2023-06-04 19:24:58 +0530

    Add files via upload

---
+++ unlock_user_lambda_function.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import json
+import boto3
+
+def lambda_handler(event, context):
+    # TODO implement
+    ssm_client = boto3.client('ssm', region_name='ap-south-1')
+    
+    cmd = { "commands": [ "usermod -U rashmi" ] }
+    
+    ssm_client.send_command(DocumentName="AWS-RunShellScript", InstanceIds=["i-01f98a6e2f16e481a"], Parameters=cmd )
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('User Unlocked!')
+    }
+
